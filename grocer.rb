712bed5afbe1b0ac467b4a257335a198538ee5dd
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-24 21:55:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ coupons.each do |coupon|
 
   if cart[name] && cart[name][:count] >= coupon[:num]
     if cart["#{name} W/COUPON"]
-      cart["#{name} W/COUPON"][:count] +=1
+      cart["#{name} W/COUPON"][:count] += 1
     else
       cart["#{name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[name][:clearance], :count => coupon[:num]}
     end
