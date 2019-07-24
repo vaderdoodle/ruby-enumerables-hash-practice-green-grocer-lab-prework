c6f270ad40ae474638950c5b9c7ef68a30f6cd41
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-24 21:18:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,22 +16,33 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-#iterate into coupons array and down into coupons hash
-coupons.each do |coupon|
-  coupon.each do |attribute, value|
-  name = coupon[:item] #makes things easier
+  #doesn't break if there is no coupon
+  return cart if coupons == []
 
-  if cart[name] && cart[name][:count] >= coupon[:num]
-    if cart["#{name} W/COUPON"]
-      cart["#{name} W/COUPON"][:count] +=1
-    else
-      cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
-    end
-  cart[name][:count] -= coupon[:num]
-  end
-  end
-end
-cart
+  #set new_cart = cart so we don't have to push all the values, just change them
+  new_cart = cart
+
+  coupons.each do |coupon|
+    name = coupon[:item] #avocado, cheese,...
+    num_of_c = coupon[:num]
+    #if the cart has the same item in coupon and has larger amount than in coupon
+    if cart.include?(name) && cart[name][:count] >= num_of_c
+       #remove number of the new_cart's count
+       new_cart[name][:count] -= num_of_c
+       #increase the count when there is more items than the coupon allows
+       if new_cart["#{name} W/COUPON"]
+         new_cart["#{name} W/COUPON"][:count] += 1
+       #set the name with coupon with new value
+       else
+         new_cart["#{name} W/COUPON"] = {
+           :price => coupon[:cost],
+           :clearance => new_cart[name][:clearance],
+           :count => 1
+         }
+       end
+     end
+   end
+   new_cart
 end
 
 def apply_clearance(cart)
