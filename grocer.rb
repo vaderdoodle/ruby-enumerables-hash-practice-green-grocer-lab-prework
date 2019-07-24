09bd0fa81bc47248da39f715cc1ab009efa16257
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-24 21:50:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ end
 def apply_coupons(cart, coupons)
 #iterate into coupons array and down into coupons hash
 coupons.each do |coupon|
-  coupon.each do |attribute, value|
+  #coupon.each do |attribute, value|
   name = coupon[:item] #makes things easier
 
   if cart[name] && cart[name][:count] >= coupon[:num]
