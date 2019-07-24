1726d75366a0c261354e627b66013ae11f203439
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-24 21:09:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,13 +21,13 @@ coupons.each do |coupon|
   coupon.each do |attribute, value|
   name = coupon[:item] #makes things easier
 
-  if cart[name] && cart[name][:count] >= coupon[:num]
-    if cart["#{name} W/COUPON"]
-      cart["#{name} W/COUPON"][:count] +=1
+  if cart[coupon[:item]] && cart[coupon[:item]][:count] >= coupon[:num]
+    if cart["#{coupon[:item]} W/COUPON"]
+      cart["#{coupon[:item]} W/COUPON"][:count] +=1
     else
-      cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
+      cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[coupon[:item]][:clearance], :count => 1}
     end
-  cart[name][:count] -= coupon[:num]
+  cart[coupon[:item]][:count] -= coupon[:num]
   end
   end
 end
